2bd9250a7fbc270e8e791da8d5e67bac8f694fd4
Luke Henderson <personal@lukeh.us>
2023-09-18 00:05:50 -0700

    requirements

---
+++ create_test.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import io, json, base64
+
+test_input = {
+    "prompt": "Drab, gloomy, desaturated, depressing, hopeless digital painting portrait of a mid-20s adventurer in with bright blonde hair and vibrant blue eyes, with a scar over his nose, face scar, scar on face medieval setting"
+}
+
+images = []
+
+with Image.open("./preprocessed.png") as image:
+    image_binary = io.BytesIO()
+    image.save(image_binary, "PNG")
+    images.append(base64.b64encode(image_binary.getvalue()).decode("utf-8"))
+
+test_input["images"] = images
+
+with open("test_input.json", "w") as json_file:
+    json.dump(test_input, json_file)
