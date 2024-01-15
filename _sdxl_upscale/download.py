cefb992f1f71a818de8f7a7a610fd499c74f0dac
Luke Henderson <personal@lukeh.us>
2024-01-15 15:03:57 -0800

    switched to esrgan for upscaling

--- _sdxl_upscale/download.py
+++ _sdxl_upscale/download.py
@@ -1,24 +1,13 @@
+from diffusers import StableDiffusionUpscalePipeline as SD
 from dotenv import load_dotenv
-from diffusers import StableDiffusionXLImg2ImgPipeline as SDXL
-from diffusers import DiffusionPipeline as Refiner
 import torch, os
 
 load_dotenv()
-SDXL_MODEL_PATH = os.getenv("SDXL_MODEL_PATH")
-SDXL_REFINER_PATH = os.getenv("SDXL_REFINER_PATH")
+SD_UPSCALE_PATH = os.getenv("SD_UPSCALE_PATH")
 
-pipe = SDXL.from_pretrained(
-    SDXL_MODEL_PATH, 
+pipe = SD.from_pretrained(
+    SD_UPSCALE_PATH, 
     torch_dtype=torch.float16,
     vaiant="fp16",
     use_safetensors=True
 )
-if(SDXL_REFINER_PATH != None and SDXL_REFINER_PATH != ""):
-    refiner = Refiner.from_pretrained(
-        SDXL_REFINER_PATH,
-        torch_dtype=torch.float16,
-        variant="fp16",
-        use_safetensors=True,
-        text_encoder_2=pipe.text_encoder_2,
-        vae=pipe.vae
-    )
--- _sdxl_upscale/upscale.py
+++ _sdxl_upscale/upscale.py
@@ -1,78 +1,73 @@
-import runpod, os, torch, base64
-from dotenv import load_dotenv
-from diffusers import StableDiffusionXLImg2ImgPipeline as SDXL
-from diffusers import DiffusionPipeline as Refiner
-from diffusers import UniPCMultistepScheduler as Scheduler
-from diffusers.utils import load_image
-from PIL import Image
-from io import BytesIO
+# from diffusers import StableDiffusionUpscalePipeline as SD
+# from dotenv import load_dotenv
+# from io import BytesIO
+# from PIL import Image
+# import runpod, os, torch, base64
 
-load_dotenv()
-SDXL_MODEL_PATH = os.getenv("SDXL_MODEL_PATH")
-SDXL_REFINER_PATH = os.getenv("SDXL_REFINER_PATH")
+# load_dotenv()
+# SD_UPSCALE_PATH = os.getenv("SD_UPSCALE_PATH")
 
-pipe = SDXL.from_pretrained(SDXL_MODEL_PATH, torch_dtype=torch.float16).to("cuda")
-pipe.scheduler = Scheduler.from_config(pipe.scheduler.config)
-pipe.enable_xformers_memory_efficient_attention()
-if(SDXL_REFINER_PATH != None and SDXL_REFINER_PATH != ""):
-    refiner = Refiner.from_pretrained(
-        SDXL_REFINER_PATH,
-        torch_dtype=torch.float16,
-        variant="fp16",
-        use_safetensors=True,
-        text_encoder_2=pipe.text_encoder_2,
-        vae=pipe.vae
-    ).to("cuda")
-    refiner.enable_xformers_memory_efficient_attention()
+# pipe = SD.from_pretrained(SD_UPSCALE_PATH, torch_dtype=torch.float16).to("cuda")
+# pipe.scheduler = Scheduler.from_config(pipe.scheduler.config)
+# pipe.enable_xformers_memory_efficient_attention()
 
-def stable_diffusion(job):
-    job_input = job["input"]
+# def stable_diffusion(job):
+#     job_input = job["input"]
 
-    image = job_input.get("image", None)
-    if(image == None):
-        return([])
-    prompt = job_input.get("prompt", None)
-    if(prompt == None):
-        return([])
-    steps = int(job_input.get("steps", 40))
-    if(refiner):
-        end_denoise = job_input.get("end_denoise", 0.8)
-    else:
-        end_denoise = 1.0
+#     image = job_input.get("image", None)
+#     if(image == None):
+#         return([])
+#     prompt = job_input.get("prompt", None)
 
-    png = Image.open(BytesIO(base64.b64decode(image))).convert("RGB")
-    supersampled = png.resize((int(png.width * 2), int(png.height * 2)))
+#     png = Image.open(BytesIO(base64.b64decode(image))).convert("RGB")
+#     supersampled = png.resize((int(png.width * 2), int(png.height * 2)))
 
-    print("Generating Image(s)...")
-    if(refiner):
-        unrefined = pipe(
-            prompt=prompt,
-            image=supersampled,
-            num_inference_steps=steps,
-            denoising_end=end_denoise,
-            strength=0.4,
-            output_type="latent"
-        ).images[0]
-        refined = refiner(
-            prompt=prompt,
-            num_inference_steps=steps,
-            denoising_start=end_denoise,
-            image=unrefined
-        ).images[0]
-    else:
-        refined = pipe(
-            prompt=prompt,
-            image=supersampled,
-            num_inference_steps=steps,
-            strength=0.4,
-        )
+#     print("Generating Image(s)...")
 
-    send_image = []
+#     refined = pipe(prompt=prompt, image=png)
 
-    with BytesIO() as image_binary:
-        refined.save(image_binary, format="PNG")
-        send_image.append(base64.b64encode(image_binary.getvalue()).decode())
+#     send_image = []
+
+#     with BytesIO() as image_binary:
+#         refined.save(image_binary, format="PNG")
+#         send_image.append(base64.b64encode(image_binary.getvalue()).decode())
     
-    return(send_image)
+#     return(send_image)
 
-runpod.serverless.start({"handler": stable_diffusion})
+# runpod.serverless.start({"handler": stable_diffusion})
+
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from realesrgan import RealESRGANer
+from io import BytesIO
+import cv2, numpy
+
+def esrgan(job):
+    job_input = job["input"]
+    scale = int(job_input.get("scale", 4))
+    image = job_input.get("image", None)
+    if(not image):
+        return([])
+    image = cv2.imdecode(numpy.frombuffer(base64.b64decode(image), numpy.uint8), cv2.IMREAD_COLOR)
+    model = RRDBNet(
+        num_in_ch=3, 
+        num_out_ch=3, 
+        num_feat=64, 
+        num_block=23, 
+        num_grow_ch=32, 
+        scale=scale
+    )
+    upsampler = RealESRGANer(
+        scale=scale, 
+        model_path="/workspace/RealESRGAN_x4plus.pth", 
+        dni_weight=None, 
+        model=model, 
+        half=True
+    )
+    output, _ = upsampler.enhance(input)
+
+    with BytesIO as image_binary:
+        cv2.imwrite(image_binary, output)
+        send_image = [base64.b64encode(image_binary.getvalue()).decode()]
+    
+
+    return(send_image)
