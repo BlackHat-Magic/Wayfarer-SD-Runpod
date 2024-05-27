2f8d3b186cc7526b12dea486728936ee68ef9e5d
Luke Henderson <personal@lukeh.us>
2024-05-27 13:55:23 -0700

    some changes

--- _sdxl_upscale/upscale.py
+++ _sdxl_upscale/upscale.py
@@ -1,22 +1,134 @@
-from diffusers import StableDiffusionXLInpaintPipeline as Inpaint
-from diffusers import StableDiffusionXLImg2ImgPipeline as SDXL
+from diffusers import StableDiffusionXLInpaintPipeline as SDXL
 from diffusers import DPMSolverMultistepScheduler as Scheduler
 from PIL import Image, ImageDraw, ImageFilter
 from dotenv import load_dotenv
-import base64, io, numpy, os, torch, runpod
+import requests, torch, base64, io, runpod
 
 load_dotenv()
 SDXL_MODEL_PATH = os.getenv("SDXL_MODEL_PATH")
 
-def stable_diffusion(job):
-    pipe = SDXL.from_pretrained(
-        SDXL_MODEL_PATH, 
-        torch_dtype=torch.float16,
-        variant="fp16",
-    ).to("cuda")
-    # pipe.enable_xformers_memory_efficient_attention()
-    pipe.scheduler = Scheduler.from_config(pipe.scheduler.config)
+pipe = SDXL.from_pretrained(
+    SDXL_MODEL_PATH,
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True
+).to("cuda")
+pipe.enable_xformers_memory_efficient_attention()
+pipe.scheduler = Scheduler.from_config(pipe.scheduler.config)
 
+def process_tile(pipe, tile: Image, steps: int, guidance: float, strength: float, mask: Image = None):
+    with torch.no_grad():
+        processed_tile = pipe(
+            prompt="",
+            num_inference_steps=steps,
+            image=tile,
+            guidance_scale=guidance,
+            strength=strength,
+            mask_image=mask
+        ).images[0]
+    return(processed_tile)
+
+def initial_sd_resample(pipe, image, tile_width, tile_height, steps, guidance, strength):
+    tiles = []
+    resampled = image.copy()
+    width, height = resampled.size
+    for i in range(0, width // (4 / 3), tile_width):
+        for j in range(0, height // (4 / 3), tile_height):
+            left_padding = 32
+            left = i - 32
+            if(left < 0):
+                left = 0
+                left_padding = 0
+            
+            top_padding = 32
+            top = j - 32
+            if(top < 0):
+                top = 0
+                top_padding = 0
+
+            right_padding = 32
+            right = i + tile_width + 32
+            if(right > width):
+                right = width
+                right_padding = 0
+
+            bottom_padding = 32
+            bottom = j + tile_height + 32
+            if(bottom > height):
+                bottom = height
+                bottom_padding = 0
+
+            mask_width = left_padding + tile_width + right_padding
+            mask_height = top_padding + tile_height + bottom_padding
+            mask = Image.new("RGB", (mask_width, mask_height), (0, 0, 0, 255))
+            draw = ImageDraw.Draw(mask)
+            draw.rectangle([left_padding, top_padding, left_padding + tile_width, top_padding + tile_height], (255, 255, 255, 255))
+
+            tile = resampled.crop((left, top, right, bottom))
+            og_width, og_height = tile.size
+            arr.append(tile.copy())
+            
+            tile = tile.resize((1024, 1024), Image.Resampling.LANCZOS)
+
+            processed_tile = process_tile(pipe, tile, steps, guidance, strength, mask)
+
+            # stitch back together
+            processed_tile = processed_tile.resize((og_width, og_height), Image.Resampling.LANCZOS)
+            processed_tile = processed_tile.crop((left_padding, top_padding, tile_width + left_padding, tile_height + top_padding))
+            resampled.paste(processed_tile, (i, j))
+    
+    return(resampled)
+
+def fix_seams(pipe, image, tile_width, tile_height, steps, guidance, strength, direction):
+    seams_fix = image.copy()
+    width, height = seams_fix.size
+    if(direction == "h"):
+        first_mask = Image.new("RGB", (tile_width * 2, tile_height))
+        draw = ImageDraw.Draw(first_mask)
+        draw.rectangle([0, tile_height // 4, tile_width * 2, tile_height - tile_height // 4], (255, 255, 255))
+        first_mask = first_mask.filter(ImageFilter.GaussianBlur(tile_width // 8))
+        first_mask = first_mask.crop((tile_width // 2, 0, tile_width + tile_width // 2, tile_height))
+    
+        subsequent_mask = Image.new("RGB", (tile_width * 2, tile_height))
+        draw = ImageDraw.Draw(subsequent_mask)
+        draw.rectangle([tile_width // 4, tile_height // 4, tile_width * 2, tile_height - tile_height // 4], (255, 255, 255))
+        subsequent_mask = subsequent_mask.filter(ImageFilter.GaussianBlur(tile_width // 8))
+        subsequent_mask = subsequent_mask.crop((0, 0, tile_width, tile_height))
+
+        width_start = 0
+        width_iter = int(tile_width * 0.75)
+        height_start = tile_height // 2
+    else:
+        first_mask = Image.new("RGB", (tile_width, tile_height * 2))
+        draw = ImageDraw.Draw(first_mask)
+        draw.rectangle([tile_width // 4, 0, tile_width - tile_width // 4, tile_height * 2], (255, 255, 255))
+        first_mask = first_mask.filter(ImageFilter.GaussianBlur(tile_width // 8))
+        first_mask = first_mask.crop((0, tile_height // 4, tile_width, tile_height // 4 + tile_height))
+
+        subsequent_mask = first_mask
+        
+        width_start = tile_width // 2
+        width_iter = tile_width
+        height_start = 0
+    for i in range(width_start, int(width / (4 / 3) + 1), width_iter):
+        for j in range(height_start, int(height // (4 / 3) + 1), tile_height):
+            left = i
+            top = j
+            right = i + tile_width
+            bottom = j + tile_height
+
+            tile = seams_fix.crop((left, top, right, bottom))
+
+            mask = subsequent_mask
+            if(i == 0):
+                mask = first_mask
+
+            processed_tile = process_tile(pipe, tile, steps, guidance, strength, mask)
+            seams_fix.paste(processed_tile, (left, top))
+    
+    return(seams_fix)
+
+def stable_diffusion(job):
     job_input = job["input"]
 
     image = job_input.get("image", None)
@@ -40,161 +152,13 @@ def stable_diffusion(job):
 
     print("Generating Image(s)...")
 
-    # upsample each tile
-    tiles = []
-    padding = 32
-    for i in range(0, width, tile_width):
-        for j in range(0, height, tile_height):
-            left_padding = 32
-            left = i - padding
-            if(left < 0):
-                left = 0
-                left_padding = 0
-            
-            top_padding = 32
-            top = j - padding
-            if(top < 0):
-                top = 0
-                top_padding = 0
-
-            right_padding = 32
-            right = i + tile_width + padding
-            if(right > width):
-                right = i + tile_width
-                right_padding = 0
-
-            bottom_padding = 32
-            bottom = j + tile_height + padding
-            if(bottom > height):
-                bottom = j + tile_height
-                bottom_padding = 0
-
-            tile = upsampled.crop((left, top, right, bottom))
-            og_width, og_height = tile.size
-            
-            tile = tile.resize((1024, 1024), Image.LANCZOS)
-
-            with torch.no_grad():
-                processed_tile = pipe(
-                    prompt="",
-                    num_inference_steps=steps,
-                    image=tile,
-                    guidance_scale=guidance,
-                    strength=strength
-                ).images[0]
-            tiles.append({
-                "left": left,
-                "top": top,
-                "left_padding": left_padding,
-                "top_padding": top_padding,
-                "og_width": og_width,
-                "og_height": og_height,
-                "image": processed_tile
-            })
-
-    # stitch them back together
-    intermediate_image = Image.new("RGB", upsampled.size)
-    for tile_obj in tiles:
-        og_width = tile_obj["og_width"]
-        og_height = tile_obj["og_height"]
-        tile = tile_obj["image"].resize((width, height), Image.LANCZOS)
-        
-        left_padding = tile_obj["left_padding"]
-        top_padding = tile_obj["top_padding"]
-        tile = tile.crop((left_padding, top_padding, og_width + left_padding, og_height + top_padding))
-
-        x = tile_obj["left"] + left_padding
-        y = tile_obj["top"] + top_padding
-        intermediate_image.paste(tile, (x, y))
-    
-    # switch to inpainting
-    torch.cuda.empty_cache()
-    pipe = Inpaint.from_pretrained(
-        SDXL_MODEL_PATH, 
-        torch_dtype=torch.float16,
-        variant="fp16",
-    ).to("cuda")
-    # pipe.enable_xformers_memory_efficient_attention()
-    pipe.scheduler = Scheduler.from_config(pipe.scheduler.config)
-
-    # generate vertical seam mask
-    vertical_mask = Image.new("RGB", (tile_width, tile_height * 2))
-    draw = ImageDraw.Draw(vertical_mask)
-    draw.rectangle([tile_width // 4, 0, tile_width // 4 + tile_width // 2, tile_height * 2], (255, 255, 255, 255))
-    vertical_mask = vertical_mask.filter(ImageFilter.GaussianBlur(128))
-    vertical_mask = vertical_mask.crop((0, tile_height // 4, tile_width, tile_height // 4 + tile_height))
-
-    # fix vertical seams
-    fixed_vertical = intermediate_image.copy()
-    for i in range(tile_width // 2, width, tile_width):
-        for j in range(0, height, tile_height):
-            left = i
-            top = j
-            right = i + tile_width
-            bottom = j + tile_height
-
-            tile = intermediate_image.crop((left, top, right, bottom))
-
-            with torch.no_grad():
-                processed_tile = pipe(
-                    prompt="",
-                    num_inference_steps=steps,
-                    image=tile,
-                    guidance_scale=guidance,
-                    strength=strength,
-                    mask_image=vertical_mask
-                ).images[0]
-            fixed_vertical.paste(processed_tile, (left, top))
-    
-    # generate horizontal masks
-    first_horizontal_mask = Image.new("RGB", (tile_width * 2, tile_height))
-    draw = ImageDraw.Draw(first_horizontal_mask)
-    draw.rectangle([0, tile_height // 4, tile_width * 2, tile_height // 4 + tile_height // 2], (255, 255, 255, 0))
-    first_horizontal_mask = first_horizontal_mask.filter(ImageFilter.GaussianBlur(128))
-    first_horizontal_mask = first_horizontal_mask.crop((tile_width // 2, 0, tile_width + tile_width // 2, tile_height))
-
-    subsequent_horizontal_mask = Image.new("RGB", (tile_width * 2, tile_height))
-    draw = ImageDraw.Draw(subsequent_horizontal_mask)
-    draw.rectangle([tile_width // 4, tile_height // 4, tile_width * 2, tile_height // 4 + tile_height // 2], (255, 255, 255, 0))
-    subsequent_horizontal_mask = subsequent_horizontal_mask.filter(ImageFilter.GaussianBlur(128))
-    subsequent_horizontal_mask = subsequent_horizontal_mask.crop((0, 0, tile_width, tile_height))
-
-    subsequent_horizontal_mask
-
-    # fix horizontal seams; making sure not to introduce new vertical ones.
-    fixed_horizontal = fixed_vertical.copy()
-    for i in range(0, width, tile_width // 2):
-        for j in range(tile_height // 2, height, tile_height):
-            left = i
-            top = j
-            right = i + tile_width
-            bottom = j + tile_height
-
-            tile = fixed_horizontal.crop((left, top, right, bottom))
-
-            if(i == 0):
-                horizontal_mask = first_horizontal_mask
-            else:
-                horizontal_mask = subsequent_horizontal_mask
-
-            with torch.no_grad():
-                processed_tile = pipe(
-                    prompt="",
-                    num_inference_steps=steps,
-                    image=tile,
-                    guidance_scale=guidance,
-                    strength=strength,
-                    mask_image=horizontal_mask
-                ).images[0]
-            fixed_horizontal.paste(processed_tile, (left, top))
-    # only a short, simple, twelve-step process
-
-    send_image = []
+    first_pass = initial_sd_resample(pipe, upsampled, tile_width, tile_height, steps, guidance, strength)
+    vertical = fix_seams(pipe, first_pass, tile_width, tile_height, steps, guidance, strength, "v")
+    horizontal = fix_seams(pipe, first_pass, tile_width, tile_height, steps, guidance, strength, "v")
 
     with io.BytesIO() as image_binary:
         fixed_horizontal.save(image_binary, format="PNG")
-        send_image.append(base64.b64encode(image_binary.getvalue()).decode())
-    
+        send_image = [base64.b64encode(image_binary.getvalue()).decode()]
     return(send_image)
 
 runpod.serverless.start({"handler": stable_diffusion})
--- build_sdxl_upscale.sh
+++ build_sdxl_upscale.sh
@@ -4,5 +4,4 @@ source .env
 
 docker builder build --no-cache \
     --build-arg "SDXL_MODEL_PATH=$SDXL_MODEL_PATH" \
-    # --build-arg "TILE_CN_MODEL_PATH=$TILE_CN_MODEL_PATH" \
     -f ./_sdxl_upscale/Dockerfile .
