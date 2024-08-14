e8e41d4933ad1be9d81d59543501f4d6cfc40d99
Luke Henderson <personal@lukeh.us>
2024-08-14 13:38:02 -0700

    rewrote upscale

--- _sdxl_upscale/upscale.py
+++ _sdxl_upscale/upscale.py
@@ -1,18 +1,29 @@
-from diffusers import StableDiffusionXLInpaintPipeline as SDXL
-from diffusers import DPMSolverMultistepScheduler as Scheduler
+from diffusers import StableDiffusionXLControlNetInpaintPipeline as SDXL
+from diffusers import EulerAncestralDiscreteScheduler as Euler
+from diffusers import ControlNetModel as CNM
+from diffusers import DDIMScheduler as DDIM
+from diffusers import AutoencoderKL as AKL
 from PIL import Image, ImageDraw, ImageFilter
 from botocore.client import Config
 from dotenv import load_dotenv
-import requests, torch, base64, io, runpod, os, boto3, random
+import torch, io, base64, boto3, random, os, runpod
 
 load_dotenv()
-SDXL_MODEL_PATH = os.getenv("SDXL_MODEL_PATH")
+
+# load SDXL Model Stuff
+SCHEDULER_MODEL_PATH = os.getenv("SDXL_UPSCALE_SCHEDULER_MODEL_PATH")
+TILE_CONTROLNET_MODEL_PATH = os.getenv("SDXL_TILE_CONTROLNET_MODEL_PATH")
+VAE_MODEL_PATH = os.getenv("SDXL_UPSCALE_VAE_MODEL_PATH")
+BASE_MODEL_PATH = os.getenv("SDXL_UPSCALE_MODEL_PATH")
+
+# Load S3 Credentials
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
 S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
 S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 S3_REGION_NAME = os.getenv("S3_REGION_NAME")
 
+# initialize S3 session
 session = boto3.session.Session()
 s3_client = session.client(
     "s3",
@@ -23,180 +34,136 @@ s3_client = session.client(
     config=Config(signature_version="s3v4")
 )
 
+# function to generate random IDs for images
 cuid = lambda x: "".join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_") for _ in range(x))
 
+# define pipeliens
+scheduler = Euler.from_pretrained(
+    SCHEDULER_MODEL_PATH,
+    subfolder="scheduler"
+)
+controlnet = CNM.from_pretrained(
+    CONTROLNET_MODEL_PATH,
+    torch_dtype=torch.float16
+)
+vae = AKL.from_pretrained(
+    VAE_MODEL_PATH,
+    torch_dtype=torch.float16
+)
 pipe = SDXL.from_pretrained(
-    SDXL_MODEL_PATH,
+    BASE_UPSCALE_MODEL_PATH,
+    controlnet=controlnet,
+    vae=vae,
+    safety_checker=None,
     torch_dtype=torch.float16,
-    variant="fp16",
-    use_safetensors=True
+    scheduler=scheduler
 ).to("cuda")
-pipe.enable_xformers_memory_efficient_attention()
-pipe.scheduler = Scheduler.from_config(pipe.scheduler.config)
 
-def process_tile(pipe, tile: Image, steps: int, guidance: float, strength: float, mask: Image = None):
-    with torch.no_grad():
-        processed_tile = pipe(
-            prompt="",
-            num_inference_steps=steps,
-            image=tile,
-            guidance_scale=guidance,
-            strength=strength,
-            mask_image=mask
-        ).images[0]
-    return(processed_tile)
+def stable_diffusion(job):
+    # get job input
+    job_input = job["input"]
+    prompt = job_input.get("prompt", "")
+    negative_prompt = job_input.get("negative_prompt", "")
+    steps = job_input.get("steps", 30)
+    scale = job_input.get("scale", 4)
+    guidance = job_input.get("guidance", 5.0)
+    mask_blur = job_input.get("mask_blur", 48)
 
-def initial_sd_resample(pipe, image, tile_width, tile_height, steps, guidance, strength):
-    tiles = []
-    resampled = image.copy()
-    width, height = resampled.size
-    for i in range(0, int(width // (4 / 3)), tile_width):
-        for j in range(0, int(height // (4 / 3)), tile_height):
-            left_padding = 32
-            left = i - 32
-            if(left < 0):
-                left = 0
-                left_padding = 0
-            
-            top_padding = 32
-            top = j - 32
-            if(top < 0):
-                top = 0
-                top_padding = 0
+    # get image
+    image_id = job_input.get("image_id", None)
+    if(image_id == None):
+        return("No image ID provided")
+    try:
+        s3_response = s3_client.get_object(Key=image_id, Bucket=S3_BUCKET_NAME)
+    except Exception as e:
+        print(e)
+        return(f"Error: {e}")
+    image_data = s3_response["Body"].read()
+    image_binary = io.BytesIO(image_data)
+    png = Image.open(image_binary)
+    upsampled = png.resize((int(png.width * scale), int(png.height * scale)), Image.Resampling.LANCZOS)
+    width, height = upsampled.size
 
-            right_padding = 32
-            right = i + tile_width + 32
-            if(right > width):
-                right = width
-                right_padding = 0
+    # create tiles and masks
+    tile_width, tile_height = png.size
+    upscaled_image = Image.new("RGB", (png.width * scale, png.height * scale))
+    for i in range(scale):
+        for j in range(scale):
+            # intial crop points
+            o_left = left = int(i * tile_width)
+            o_top = top = int(j * tile_height)
+            o_right = right = left + tile_width
+            o_bottom = bottom = top + tile_height
 
-            bottom_padding = 32
-            bottom = j + tile_height + 32
-            if(bottom > height):
-                bottom = height
-                bottom_padding = 0
+            # mask boundaries
+            b_left = b_top = 0
 
-            mask_width = left_padding + tile_width + right_padding
-            mask_height = top_padding + tile_height + bottom_padding
+            # mask size
+            mask_width = tile_width
+            mask_height = tile_height
+
+            # mask blur
+            if(i > 0):
+                top -= mask_blur
+                b_top += mask_blur
+                mask_height += mask_blur
+            if(i < scale - 1):
+                bottom += mask_blur
+                mask_height += mask_blur
+            if(j > 0):
+                left -= mask_blur
+                b_left += mask_blur
+                mask_width += mask_blur
+            if(j < scale - 1):
+                right += mask_blur
+                mask_width += mask_blur
+
+            # mask boundaries again
+            b_right = b_left + tile_width
+            b_bottom = b_top + tile_height
+
+            # create tile
+            tile = upsampled.crop((left, top, right, bottom))
+
+            # create mask
             mask = Image.new("RGB", (mask_width, mask_height), (0, 0, 0, 255))
             draw = ImageDraw.Draw(mask)
-            draw.rectangle([left_padding, top_padding, left_padding + tile_width, top_padding + tile_height], (255, 255, 255, 255))
+            draw.rectangle([b_left, b_top, b_right, b_bottom], (255, 255, 255, 255))
+            mask = mask.filter(ImageFilter.GaussianBlur(mask_blur))
 
-            tile = resampled.crop((left, top, right, bottom))
-            og_width, og_height = tile.size
-            
-            tile = tile.resize((1024, 1024), Image.Resampling.LANCZOS)
+            # resample tile
+            resampled = pipe(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                num_inference_steps=steps,
+                guidance=guidance,
+                image=tile,
+                control_image=tile,
+                mask_image=mask
+            ).images[0]
 
-            processed_tile = process_tile(pipe, tile, steps, guidance, strength, mask)
-
-            # stitch back together
-            processed_tile = processed_tile.resize((og_width, og_height), Image.Resampling.LANCZOS)
-            processed_tile = processed_tile.crop((left_padding, top_padding, tile_width + left_padding, tile_height + top_padding))
-            resampled.paste(processed_tile, (i, j))
+            # paste
+            cropped_resample = resampled.crop((b_left, b_top, b_right, b_bottom))
+            upscaled_image.paste(cropped_resample, (o_left, o_top, o_right, o_bottom))
     
-    return(resampled)
-
-def fix_seams(pipe, image, tile_width, tile_height, steps, guidance, strength, direction):
-    seams_fix = image.copy()
-    width, height = seams_fix.size
-    if(direction == "h"):
-        first_mask = Image.new("RGB", (tile_width * 2, tile_height))
-        draw = ImageDraw.Draw(first_mask)
-        draw.rectangle([0, tile_height // 4, tile_width * 2, tile_height - tile_height // 4], (255, 255, 255))
-        first_mask = first_mask.filter(ImageFilter.GaussianBlur(tile_width // 8))
-        first_mask = first_mask.crop((tile_width // 2, 0, tile_width + tile_width // 2, tile_height))
-    
-        subsequent_mask = Image.new("RGB", (tile_width * 2, tile_height))
-        draw = ImageDraw.Draw(subsequent_mask)
-        draw.rectangle([tile_width // 4, tile_height // 4, tile_width * 2, tile_height - tile_height // 4], (255, 255, 255))
-        subsequent_mask = subsequent_mask.filter(ImageFilter.GaussianBlur(tile_width // 8))
-        subsequent_mask = subsequent_mask.crop((0, 0, tile_width, tile_height))
-
-        width_start = 0
-        width_iter = int(tile_width * 0.75)
-        height_start = tile_height // 2
-    else:
-        first_mask = Image.new("RGB", (tile_width, tile_height * 2))
-        draw = ImageDraw.Draw(first_mask)
-        draw.rectangle([tile_width // 4, 0, tile_width - tile_width // 4, tile_height * 2], (255, 255, 255))
-        first_mask = first_mask.filter(ImageFilter.GaussianBlur(tile_width // 8))
-        first_mask = first_mask.crop((0, tile_height // 4, tile_width, tile_height // 4 + tile_height))
-
-        subsequent_mask = first_mask
-        
-        width_start = tile_width // 2
-        width_iter = tile_width
-        height_start = 0
-    for i in range(width_start, int(width / (4 / 3) + 1), width_iter):
-        for j in range(height_start, int(height // (4 / 3) + 1), tile_height):
-            left = i
-            top = j
-            right = i + tile_width
-            bottom = j + tile_height
-
-            tile = seams_fix.crop((left, top, right, bottom))
-
-            mask = subsequent_mask
-            if(i == 0):
-                mask = first_mask
-
-            processed_tile = process_tile(pipe, tile, steps, guidance, strength, mask)
-            seams_fix.paste(processed_tile, (left, top))
-    
-    return(seams_fix)
-
-def upload_image(image, client, bucket, object_name):
+    # upload image
     try:
         image_binary = io.BytesIO()
-        image.save(image_binary, format="PNG")
+        image.save(image_binary, format="png")
         image_binary.seek(0)
-
-        client.put_object(
-            Key=object_name, 
-            Bucket=bucket, 
-            Body=image_binary, 
+        key = cuid(8)
+        s3_client.put_object(
+            Key=f"{key}.png",
+            Bucket=S3_BUCKET_NAME,
+            Body=image_binary,
             ContentType="image/png",
             ACL="public-read"
         )
-        return(f"{bucket}/{object_name}")
     except Exception as e:
         print(e)
-        return(0)
-
-def stable_diffusion(job):
-    job_input = job["input"]
-
-    image = job_input.get("image", None)
-    if(image == None):
-        return([])
-    prompt = job_input.get("prompt", None)
-    negative_prompt = job_input.get("negative_prompt", None)
-    steps = job_input.get("steps", 6)
-    strength = job_input.get("strength", 0.5)
-    scale = job_input.get("scale", 4.0)
-    guidance = job_input.get("guidance", 2.0)
-
-    png = Image.open(io.BytesIO(base64.b64decode(image))).convert("RGB")
-    upsampled = png.resize((int(png.width * scale), int(png.height * scale)), Image.LANCZOS)
-    width, height = upsampled.size
-
-    tile_width, tile_height = width, height
-    while tile_width * tile_height > 1024 ** 2:
-        tile_width //= 2
-        tile_height //= 2
-
-    print("Generating Image(s)...")
-
-    first_pass = initial_sd_resample(pipe, upsampled, tile_width, tile_height, steps, guidance, strength)
-    vertical = fix_seams(pipe, first_pass, tile_width, tile_height, steps, guidance, strength, "v")
-    horizontal = fix_seams(pipe, first_pass, tile_width, tile_height, steps, guidance, strength, "h")
-
-    object_endpoint = upload_image(horizontal, s3_client, S3_BUCKET_NAME, f"{cuid(8)}.png")
-    if(object_endpoint == 0):
-        full_path = "Error"
-    else:
-        full_path = f"{S3_ENDPOINT_URL}/{object_endpoint}"
-
-    return(full_path)
+        return(f"Error: {e}")
+    
+    # return image URL
+    return(f"{S3_ENDPOINT_URL}/{S3_BUCKET_NAME}/{key}.png")
 
 runpod.serverless.start({"handler": stable_diffusion})
--- build_flux_generic.sh
+++ build_flux_generic.sh
@@ -4,4 +4,5 @@ source .env
 
 docker buildx build --no-cache \
     --build-arg "FLUX_MODEL_PATH=$FLUX_MODEL_PATH" \
+    --build-arg "HUGGINGFACE_API_TOKEN=$HUGGINGFACE_API_TOKEN" \
     -f ./_flux_generic/Dockerfile .
