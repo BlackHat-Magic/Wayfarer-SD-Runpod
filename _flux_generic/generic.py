d5fda2231703e1b61e6d5b60dcc628bb9f04f6f4
Luke Henderson <personal@lukeh.us>
2024-08-14 18:04:49 -0700

    rewrote flux generic

--- _flux_generic/generic.py
+++ _flux_generic/generic.py
@@ -1,19 +1,41 @@
-import runpod, os, torch, base64
-from dotenv import load_dotenv
 from diffusers import FluxPipeline as Flux
+from botocore.client import Config
 from huggingface_hub import login
+from dotenv import load_dotenv
 from PIL import Image
-from io import BytesIO
+import base64, boto3, io, os, random, runpod, torch
 
 load_dotenv()
-FLUX_MODEL_PATH = os.getenv("FLUX_MODEL_PATH")
+BASE_MODEL_PATH = os.getenv("FLUX_GENERIC_MODEL_PATH")
+
 HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+
+S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
+S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+S3_REGION_NAME = os.getenv("S3_REGION_NAME")
+S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL")
+
+# initialize S3 session
+session = boto3.session.Session()
+s3_client = session.client(
+    "s3",
+    region_name=S3_REGION_NAME,
+    endpoint_url=S3_ENDPOINT_URL,
+    aws_access_key_id=S3_ACCESS_KEY,
+    aws_secret_access_key=S3_SECRET_KEY,
+    config=Config(signature_version="s3v4")
+)
+
+# function to generate random IDs for images
+cuid = lambda x: "".join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_") for _ in range(x))
+
 login(HUGGINGFACE_API_TOKEN)
 
 pipe = Flux.from_pretrained(
-    FLUX_MODEL_PATH, 
-    torch_dtype=torch.float16, 
-    use_safetensors=True
+    BASE_MODEL_PATH,
+    safety_checker=None,
+    torch_dtype=torch.bfloat16,
 )
 pipe.enable_model_cpu_offload()
 
@@ -22,11 +44,11 @@ def flux(job):
 
     prompt = job_input.get("prompt", "A cat using a toaster.")
     # negative_prompt = job_input.get("negative_prompt", "bad quality, worst quality, blurry, out of focus, cropped, out of frame, deformed, bad hands, bad anatomy")
-    height = job_input.get("height", 1024)
-    width = job_input.get("width", 1024)
-    steps = job_input.get("steps", 50)
-    guidance = job_input.get("guidance", 3.5)
-    num_images = job_input.get("num_images", 4)
+    height = job_input.get("height", 1440)
+    width = job_input.get("width", 1440)
+    steps = job_input.get("steps", 30)
+    guidance = job_input.get("guidance", 5.0)
+    num_images = job_input.get("num_images", 1)
 
     # denoising_strength = job_input.get("denoising_strength", 0)
     # seed = job_input.get("seed", -1)
@@ -43,15 +65,28 @@ def flux(job):
         num_inference_steps=steps,
         guidance_scale=guidance,
         num_images_per_prompt=num_images,
+        # clip_skip=-2
     ).images
 
-    send_image = []
-
+    urls = []
     for image in images:
-        buffer = BytesIO()
-        image.save(buffer, format="PNG")
-        send_image.append(base64.b64encode(buffer.getvalue()).decode())
-    
-    return(send_image)
+        image_binary = io.BytesIO()
+        image.save(image_binary, format="PNG")
+        image_binary.seek(0)
+        key = cuid(8)
+        try:
+            s3_client.put_object(
+                Key=f"{key}.png",
+                Bucket=S3_BUCKET_NAME,
+                Body=image_binary,
+                ContentType="image/png",
+                ACL="public-read"
+            )
+            urls.append(f"{S3_ENDPOINT_URL}/{S3_BUCKET_NAME}/{key}.png")
+        except Exception as e:
+            print(e)
+            urls.append(f"Error: {e}")
+
+    return(urls)
 
 runpod.serverless.start({"handler": flux})
--- build_flux_generic.sh
+++ build_flux_generic.sh
@@ -3,6 +3,11 @@
 source .env
 
 docker buildx build --no-cache \
-    --build-arg "FLUX_MODEL_PATH=$FLUX_MODEL_PATH" \
+    --build-arg "FLUX_GENERIC_MODEL_PATH=$FLUX_GENERIC_MODEL_PATH" \
     --build-arg "HUGGINGFACE_API_TOKEN=$HUGGINGFACE_API_TOKEN" \
+    --build-arg "S3_ACCESS_KEY=$S3_ACCESS_KEY" \
+    --build-arg "S3_SECRET_KEY=$S3_SECRET_KEY" \
+    --build-arg "S3_BUCKET_NAME=$S3_BUCKET_NAME" \
+    --build-arg "S3_REGION_NAME=$S3_REGION_NAME" \
+    --build-arg "S3_ENDPOINT_URL=$S3_ENDPOINT_URL" \
     -f ./_flux_generic/Dockerfile .
