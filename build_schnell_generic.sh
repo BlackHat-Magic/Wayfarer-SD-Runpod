e1645772f4a4a78e581eec78cb25d14edaed886f
Luke Henderson <personal@lukeh.us>
2024-08-18 15:59:12 -0700

    oops

--- build_schnell_generic.sh
+++ build_schnell_generic.sh
@@ -4,4 +4,4 @@ source .env
 
 docker buildx build --no-cache \
     --build-arg "FLUX_SCHNELL_MODEL_PATH=$FLUX_SCHNELL_MODEL_PATH" \
-    -f ./_flux_generic/Dockerfile .
+    -f ./_schnell_generic/Dockerfile .
